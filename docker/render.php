26ff112f8401b5a5b3265f08074272d90cbcb327
kw300908 <kw300908@student.polsl.pl>
2025-12-04 18:27:51 +0100

    Update render.php

--- docker/render.php
+++ docker/render.php
@@ -1,1 +1,15 @@
+<?php
+require_once __DIR__.'/prestacore/bootstrap.php';
+
+$template = new HTMLTemplateInvoice($exampleOrder, $smarty);
+
+$pdf = new PDFGenerator();
+$pdf->setFontForLang('en');
+$pdf->startPageGroup();
+$pdf->createHeader($template->getHeader());
+$pdf->createContent($template->getContent());
+$pdf->createFooter($template->getFooter());
+$pdf->writePage();
+
+$pdf->output('preview.pdf', 'I');
 
